c26c77ad25311ee7ecbebed9ed0a47f1d86fe1ea
tikhonenkov_ks <tihonenkovk@mail.ru>
2025-07-03 19:00:32 +0700

    Add comments

--- client-interfaces/src/main/java/ru/qa/api/client/interfaces/Client.java
+++ client-interfaces/src/main/java/ru/qa/api/client/interfaces/Client.java
@@ -4,5 +4,11 @@ package ru.qa.api.client.interfaces;
  * Основной интерфейс клиента, для выполнения подготовленных запросов
  */
 public interface Client {
+    /**
+     * Выполнить переданный запрос
+     * @param request - запрос со всей необходимой информацией для его выполнения {@link Request}.
+     *                Можно сформировать либо через сеттеры
+     * @return - полученный ответ от сервиса на отправленный запрос
+     */
     ResponseActions execute(Request request);
 }
--- client-interfaces/src/main/java/ru/qa/api/client/interfaces/ResponseActions.java
+++ client-interfaces/src/main/java/ru/qa/api/client/interfaces/ResponseActions.java
@@ -1,9 +1,38 @@
 package ru.qa.api.client.interfaces;
 
+/**
+ * Действия, которые можно выполнить над ответом на запрос
+ */
 public interface ResponseActions {
-    <T> T parseBodyAs(Class<T> entity);
-    <T> T getValueFromBodyByPath(String filter);
+    /**
+     * "Распарсить" текстовое тело ответа (если оно текстовое)
+     * @param tClass - сущность, на которую надо парсить тело ответа
+     * @return - объект класса tClass с данными из тела ответа
+     */
+    <T> T parseBodyAs(Class<T> tClass);
+
+    /**
+     * Получить значение из поля тела ответа, найденного по пути
+     * @param path - путь до ключа в теле ответа
+     * @return - значение из поля ответа
+     */
+    <T> T getValueFromBodyByPath(String path);
+
+    /**
+     * Проверить код ответа на соответствие ожидаемому коду. Если ожидаемое значение не соответствует действительному,
+     * то тест упадёт по аналогии с ошибкой из Assertions.assertEquals() в JUnit5 (использован аналогичный Error)
+     * @param expectedStatusCode - ожидаемый статус - код
+     * @return - объект с ответом на запрос
+     */
     ResponseActions shouldBeStatusCode(int expectedStatusCode);
+
+    /**
+     * Получить статус - код ответа на запрос
+     */
     int getStatusCode();
+
+    /**
+     * @return - тело запроса в строковом представлении
+     */
     String getBody();
 }
